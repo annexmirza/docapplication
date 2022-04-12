1fdaf5d2c84852cf36f5e3b608bf2e6531710d3d
Malik Talha <talha.amin0909@gmail.com>
2022-04-12 15:10:14 +0500

    get file

--- lib/controllers/conversion_controller.dart
+++ lib/controllers/conversion_controller.dart
@@ -1,20 +1,25 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ConversionController extends GetxController{
 
-
-  getFile({required FileType fileType}) async{
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      allowCompression: true,
-      type: fileType,
-    );
-    if (result != null) {
-      var file = File(result.files.single.path!);
-    } else {
-      // User canceled the picker
+  Future<File?> getFile({required FileType fileType}) async{
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        allowCompression: true,
+        type: fileType,
+      );
+      if (result != null) {
+        var file = File(result.files.single.path!);
+        return file;
+      }
+      return null;
+    }catch(e){
+      Get.snackbar('Error', e.toString(),backgroundColor: Colors.black,colorText: Colors.white);
+      return null;
     }
   }
 
